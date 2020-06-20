83b4044d06eab16dbfa2fd71659c8a48be921c63
Wasim Dhamriya <56508640+wasimdhamriya@users.noreply.github.com>
2020-06-20 20:48:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I'm Wasim Dhamriya
+          I'm Wasim
         </p>
         <a
           className="App-link"
